68fac7cbc7ada1eadd66d4a647965fd69a50bb65
David Wagner <mnem@noiseandheat.com>
2013-01-20 12:27:18 +0000

    Simple grid of items

--- Assets/Scripts/BoardState.cs
+++ Assets/Scripts/BoardState.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 using System.Collections;
 
 public class BoardState : MonoBehaviour {
-
-	Vector2 cellCount;
+	
+	public GameObject template;
+	public Vector2 cellCount;
 
 	private Cell[,] cells;
 	
 	
 	// Use this for initialization
 	void Start () {
+		initialiseCells();
 		
+		Vector3 current = new Vector3(-5, 0, -1);
+		float displayWidth = 1;//template.renderer.bounds.extents.x;
+		float displayHeight = 1;//template.renderer.bounds.extents.y;
+		
+		for(int y = 0; y < cellCount.y; ++y) {
+			for(int x = 0; x < cellCount.x; ++x) {
+				GameObject display = (GameObject)Instantiate(template, current, Quaternion.identity);
+				Cell cell = new Cell() {item = display};
+				cells[x,y] = cell;
+				current.x += displayWidth;
+			}
+			current.y += displayHeight;
+			current.x = -5;
+		}
 	}
 	
 	void Reset() {
@@ -46,5 +62,6 @@ public class BoardState : MonoBehaviour {
 		
 		public Type type = Type.Null;
 		public Flavour flavour = Flavour.Null;
+		public GameObject item;
 	}
 }
