d389be73fcf793e222cd4d3e4e57e4490804c917
David Wagner <mnem@noiseandheat.com>
2013-01-28 23:02:10 +0000

    Add basic pivot point support for match rotations

--- Assets/Scripts/BlockState.cs
+++ Assets/Scripts/BlockState.cs
@@ -16,33 +16,33 @@ public class BlockState : MonoBehaviour {
 		Yellow
 	}
 	
-	private Colour _currentColour;
-	public Colour colour {
-		get { return _colour; }
-		set {
-			if (value != _colour) {
-				_colour = value;
-			}	
-		}
+	public enum VisualPivotPoint {
+		Centre,
+		LeftCentre,
+		RightCentre,
+		TopCentre,
+		BottomCentre,
+		ForceRefresh // Special value which just cause anchor recalculation
 	}
 	
-	private Flavour _currentFlavour = Flavour.Null;
-	public Flavour flavour {
-		get { return _flavour; }
-		set {
-			if (value != _flavour) {
-				_flavour = value;
-			}	
-		}
-	}
+	public Colour colour { get; set; }
+	public Flavour flavour { get; set; }
+	public VisualPivotPoint visualPivotPoint  { get; set; }
+	
+	// These fields store the actual state. This is used
+	// to switch to the requested state on the next update
+	// as necessary.
+	private Colour _currentColour;
+	private Flavour _currentFlavour;
+	private VisualPivotPoint _currentVisualPivotPoint;
+	
 	
 	// Use this for initialization
 	void Start () {
 		GameObject core = GameObject.Find("GameCore");
 		_map = core.GetComponent<BlockTypeVisualMap>();
 		
-		_currentColour = _colour = Colour.Null;
-		_currentFlavour = _flavour = Flavour.Null;
+		_currentVisualPivotPoint = VisualPivotPoint.Centre;
 		
 		RandomFlavour();
 		RandomColour();
@@ -51,6 +51,7 @@ public class BlockState : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		UpdateVisualMesh();
+		UpdatePivotPoint();
 		UpdateVisualColour();
 	}
 
@@ -81,11 +82,54 @@ public class BlockState : MonoBehaviour {
 			break;
 		}
 	}
+
+	void UpdatePivotPoint ()
+	{
+		if (visualPivotPoint == _currentVisualPivotPoint) return;
+		
+		if (visualPivotPoint == VisualPivotPoint.ForceRefresh) {
+			// Restore the real pivot. This is a little
+			// race condition-y but will probably work ok
+			visualPivotPoint = _currentVisualPivotPoint;
+		}
+		
+		Vector3 pivot = new Vector3(0f, 0f, 0f);
+		
+		switch (visualPivotPoint) {
+		case VisualPivotPoint.Centre:
+			// By default it's centered, 0, 0, 0
+			break;
+		case VisualPivotPoint.LeftCentre:
+			pivot.x = -renderer.bounds.extents.x;
+			break;
+		case VisualPivotPoint.RightCentre:
+			pivot.x = renderer.bounds.extents.x;
+			break;
+		case VisualPivotPoint.TopCentre:
+			pivot.y = renderer.bounds.extents.y;
+			break;
+		case VisualPivotPoint.BottomCentre:
+			pivot.y = -renderer.bounds.extents.y;
+			break;
+		}
+		
+		_visualContainer.transform.localPosition = pivot;
+
+		// Offset the visuals so they are back in the
+		// centre of the cell
+		pivot.x *= -1;
+		pivot.y *= -1;
+		pivot.z *= -1;
+		if (flavour == Flavour.Cone) pivot.y += -0.45f;
+		_visual.transform.localPosition = pivot;
+		
+		_currentVisualPivotPoint = visualPivotPoint;
+	}
 	
 	private void UpdateVisualColour() {
-		if (_currentColour == _colour) return;
+		if (_currentColour == colour) return;
 		
-		switch (_colour) {
+		switch (colour) {
 		case Colour.Blue:
 			_visual.renderer.material = _map.colourBlue;
 			break;
@@ -95,16 +139,13 @@ public class BlockState : MonoBehaviour {
 		case Colour.Yellow:
 			_visual.renderer.material = _map.colourYellow;
 			break;
-		default:
-			Debug.Log("Unrecognised colour! " + _colour);
-			break;
 		}
 		
-		_currentColour = _colour;
+		_currentColour = colour;
 	}
 	
 	private void UpdateVisualMesh() {
-		if (_currentFlavour == _flavour) return;
+		if (_currentFlavour == flavour) return;
 		
 		if (_visualContainer == null) {
 			// Create the container for the visuals
@@ -118,7 +159,7 @@ public class BlockState : MonoBehaviour {
 			_visual = null;
 		}
 		
-		switch (_flavour) {
+		switch (flavour) {
 		case Flavour.Cone:
 			_visual = (GameObject)Instantiate(_map.flavourCone);
 			break;
@@ -128,22 +169,13 @@ public class BlockState : MonoBehaviour {
 		case Flavour.Sphere:
 			_visual = (GameObject)Instantiate(_map.flavourSphere);
 			break;
-		default:
-			Debug.Log("Unrecognised flavour! " + _flavour);
-			break;
 		}
 		
 		_visual.transform.parent = _visualContainer.transform;
 		
-		if (_flavour == Flavour.Cone) {
-			_visual.transform.localPosition = new Vector3(0f, -0.45f, 0f);
-		} else {
-			_visual.transform.localPosition = new Vector3(0f, 0f, 0f);
-		}
+		visualPivotPoint = VisualPivotPoint.ForceRefresh;
 		
-		_currentFlavour = _flavour;
-		
-		UpdateVisualColour();
+		_currentFlavour = flavour;
 	}
 
 	public GameObject visualContainer {
@@ -158,8 +190,6 @@ public class BlockState : MonoBehaviour {
 		}
 	}
 
-	private Colour _colour;
-	private Flavour _flavour;
 	private GameObject _visualContainer;
 	private GameObject _visual;
 	private BlockTypeVisualMap _map;
